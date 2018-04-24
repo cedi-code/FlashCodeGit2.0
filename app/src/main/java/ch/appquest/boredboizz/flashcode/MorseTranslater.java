f43facf395deb2d727de84c24594d0ffcec5f1e2
Seedräck <Leuenberger.florian@gmx.ch>
2018-04-24 15:14:41 +0200

    encoder nicht fertig
    dasgf

--- app/src/main/java/ch/appquest/boredboizz/flashcode/MorseTranslater.java
+++ app/src/main/java/ch/appquest/boredboizz/flashcode/MorseTranslater.java
@@ -38,11 +38,58 @@ public class MorseTranslater {
     }
 
     public MorseTranslater() {
+
         initMorseTable();
     }
 
-    public String morseToText(int[] morse) {
+    public String addSpace(int count) {
+        String zeichen = "";
+        if (count > 1 & count <= 4) {
+            zeichen = ",";
+        }
+        else if (count > 4) {
+            zeichen = "?";
+        }
 
+        return zeichen;
+    }
+
+    public String addChar(int count) {
+        String zeichen = "";
+        if (count > 1 & count < 4) { //Fals nur 1 -4 einsen hat dann ein -> .
+            zeichen += ".";
+            count = 0; //stueck zurücksetzen
+        }
+        else if (count > 4) { //Fals mehr als 4 dann ein -> -
+            zeichen += "-";
+            count = 0; //stueck zurücksetzen
+        }
+
+        return zeichen;
+    }
+
+    public String morseToText(int[] morse) {
+        int count0 = 0;
+        int count1 = 0;
+        String satz = "";
+
+        for(int i=0; i<morse.length; i++)
+        {
+            if (morse[i] == 1) { //Einsen zählen
+                count1++;
+                if (morse[i] + 1 == 0) {
+                    satz = addChar(count1);
+                    count1 = 0; //count1 zurücksetzen
+                }
+            }
+            else if (morse[i] == 0) { // Nullen zählen
+                count0++;
+                if (morse[i] + 1 == 1) {
+                    satz = addSpace(count0);
+                    count0 = 0; //count0 zurücksetzen
+                }
+            }
+        }
 
         return "Dr Rückgabewärt";
     }
